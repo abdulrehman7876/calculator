8e89e6e241ef358dc0833ea026952e688750c76d
Abdul Rehman <145670921+abdulrehman7876@users.noreply.github.com>
2025-05-31 18:50:18 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
 let button = document.querySelectorAll(".button");
-// let numbers = document.querySelectorAll(".num");
 let mainScreen = document.querySelector(".main-screen");
 let equalTo = document.querySelector("#equal-to");
 let clear = document.querySelector("#clear")
@@ -28,26 +27,10 @@ button.forEach((butt)=>{
 button.forEach((butt)=>{
     butt.addEventListener('click', ()=>{
         string = string + butt.innerText;
-        console.log(string);
         mainScreen.innerText = string;
     })
 })
 
-// numbers.forEach((num) => {
-//     num.addEventListener("click", ()=>{
-//         eachNum.push(num.innerText);
-//         console.log(eachNum);
-//         mainScreen.innerText === '0'?  mainScreen.innerText = num.innerText :  mainScreen.innerText =  mainScreen.innerText + num.innerText;
-//     })
-// })
-
-// operator.forEach((opr) => {
-//     opr.addEventListener("click", ()=>{
-//         eachOpr = opr.innerText;
-//         console.log(eachOpr);
-//         mainScreen.innerText =  mainScreen.innerText + opr.innerText;
-//     })
-// })
 
 equalTo.addEventListener("click", ()=> {
     try {
@@ -56,30 +39,9 @@ equalTo.addEventListener("click", ()=> {
     catch (error) {
         mainScreen.innerText = "Error!";
     }
-    // mainScreen.innerText = eval(string);
-    // eachNum = [result];
-    // mainScreen.innerText = result;
-    // mainScreen.innerText = result2;
+
 })
 
 clear.addEventListener("click", ()=>{
     reset();
 });
-
-
-
-
-
-// operator.forEach((opr) => {
-//     opr.addEventListener("click", ()=>{
-//         eachOpr = opr.innerText;
-//         mainScreen.innerText = mainScreen.innerText + eachOpr;
-//     })
-// });
-// numbers.forEach((num) => {
-//     num.addEventListener("click", ()=>{
-//         eachNum = num.innerText;
-//         mainScreen.innerText = mainScreen.innerText + eachNum;
-//         console.log(eachNum, eachOpr);
-//     })
-// });
